c3d3efff6fa5ebd871f1e79a1a9eefe509cecd62
oluwaseunolusanya <oluwaseun_olusanya@yahoo.com>
2023-08-11 10:12:50 +0100

    Added a new book to the collection and set out two favourites.

--- js-institute/essential-1/module-2/books.js
+++ js-institute/essential-1/module-2/books.js
@@ -16,4 +16,20 @@ let books = [
         pages: 352
     }    
 ];
-console.log(books[0].title);
+//console.log(books[0].title);
+
+let newbook = {
+    title: "Learning JavaScript Design Patterns",
+    author: "Addy Osmani",
+    pages: 254
+};
+books.push(newbook);
+
+console.log(books.length);
+for(let book of books){
+    console.log(book.title);
+};
+
+let favourites = books.slice(-2);
+console.log(favourites.length);
+console.log(favourites);
