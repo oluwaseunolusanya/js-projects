a57c144b40f1faadeb5f7e4ea5ed9cb604084c1e
oluwaseunolusanya <oluwaseun_olusanya@yahoo.com>
2023-08-11 09:53:27 +0100

    Creating a collection of JavaScript books.

---
+++ js-institute/essential-1/module-2/books.js
@@ -0,0 +1,19 @@
+//Small collection of Javascripts books.
+let books = [
+    {
+        title: "Speaking JavaScript",
+        author: "Axel Rauschmayer",
+        pages: 460 
+    },
+    {
+        title: "Programming JavaScript",
+        author: "Eric Elliot",
+        pages: 254
+    },
+    {
+        title: "Understanding ECMAScript 6",
+        author: "Nic Zakas",
+        pages: 352
+    }    
+];
+console.log(books[0].title);
